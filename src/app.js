a0f961a11df417881b6d11460828a250e88728f9
Your Name <you@example.com>
2021-10-19 00:29:16 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+console.log("merhaba kodlama.io")
+let dolarDun=9.20
+dolarDun="ankara"
+let dolarBugun=9.30
+console.log(dolarBugun)
+
+const euro=11
+
+//euro=12  bunu yapamayız const olduğu için.
+
+//array
+let konutKredileri=["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi"]
+console.log("<ul>")
+for(let i=0;i<3;i++){
+    console.log("<li>"+ konutKredileri[i]+"</li>")
+}0
+console.log("</ul>")
+
